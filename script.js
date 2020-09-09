104901429f72ffca64fbec7519740600ad25786f
Pedro <pedrogabe@gmail.com>
2020-09-09 12:54:02 -0300

    Variable "paused" agregada

--- script.js
+++ script.js
@@ -1,52 +1,54 @@
-var iteration=0, instance=1, direction=1, goingLeft=false, goingRight=false, jumping=false, goingUp=false, playerY=0, playerX=1;
+var paused=false, iteration=0, instance=1, direction=1, goingLeft=false, goingRight=false, jumping=false, goingUp=false, playerY=0, playerX=1;
 const tRex = document.querySelector('.t-rex');
 var interval = window.setInterval(function(){
-    iteration++;
-    if(jumping){
-        instance=1;
-        if(goingUp==false && playerY==0){
-            goingUp=true;
-        }
-        if(playerY<100 && goingUp==true){
-            playerY+=4
-        }else{ 
-            if(playerY==100){
-                goingUp=false;
-            }
-            if(playerY>4 && goingUp==false){
-                playerY-=4
-            }else if(playerY==4){
-                jumping=false;
-                playerY-=4;
-            }
-        }
-        tRex.style.bottom=playerY+'px';
-    }else
-    if(goingLeft==false && goingRight==false || goingLeft==true && goingRight==true){
-        instance=0;
-    }
-    else {
-        if(iteration/4 % 2 === 0){
+    if(!paused){
+        iteration++;
+        if(jumping){
             instance=1;
-        }else if (iteration/4 % 1 === 0){
-            instance=2;
+            if(goingUp==false && playerY==0){
+                goingUp=true;
+            }
+            if(playerY<100 && goingUp==true){
+                playerY+=4
+            }else{ 
+                if(playerY==100){
+                    goingUp=false;
+                }
+                if(playerY>4 && goingUp==false){
+                    playerY-=4
+                }else if(playerY==4){
+                    jumping=false;
+                    playerY-=4;
+                }
+            }
+            tRex.style.bottom=playerY+'px';
+        }else
+        if(goingLeft==false && goingRight==false || goingLeft==true && goingRight==true){
+            instance=0;
         }
-    }
-    tRex.src="trex-"+instance+".png";
+        else {
+            if(iteration/4 % 2 === 0){
+                instance=1;
+            }else if (iteration/4 % 1 === 0){
+                instance=2;
+            }
+        }
+        tRex.src="trex-"+instance+".png";
 
-    if(direction==1){
-        tRex.style.transform='rotateY(0deg)'
-    }else{
-        tRex.style.transform='rotateY(180deg)';
-    }
-    
-    if(goingLeft==true && goingRight==false && playerX>0){
-        playerX-=5;
-        tRex.style.left=playerX+"px";
-    }
-    if(goingLeft==false && goingRight==true && playerX<(document.body.clientWidth-tRex.width)){
-        playerX+=5;
-        tRex.style.left=playerX+"px";
+        if(direction==1){
+            tRex.style.transform='rotateY(0deg)'
+        }else{
+            tRex.style.transform='rotateY(180deg)';
+        }
+        
+        if(goingLeft==true && goingRight==false && playerX>0){
+            playerX-=5;
+            tRex.style.left=playerX+"px";
+        }
+        if(goingLeft==false && goingRight==true && playerX<(document.body.clientWidth-tRex.width)){
+            playerX+=5;
+            tRex.style.left=playerX+"px";
+        }
     }
 },25);
 
